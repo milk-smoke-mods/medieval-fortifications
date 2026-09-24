d3a6d4680ca207a1d4cda4dada35f0fb7740390f
Andrew Schaeffer <andrew.jon.schaeffer@gmail.com>
2026-09-24 00:13:55 -0400

    Continuing

--- src/main/java/io/github/milk_smoke_mods/medieval_fortifications/collections/Maps.java
+++ src/main/java/io/github/milk_smoke_mods/medieval_fortifications/collections/Maps.java
@@ -11,6 +11,19 @@ import java.util.Map;
 
 public record Maps() {
     /**
+     * Stores block families according to material
+     */
+    public static Map<MaterialFamilies, List<DeferredBlock<EmbrasureBlock>>> MATERIAL_FAMILIES =
+            Collections.unmodifiableMap(new EnumMap<>(MaterialFamilies.class) {{
+                    for(MaterialFamilies materialFamily : MaterialFamilies.values()) {
+                        for (BlockTypes blockType : BlockTypes.values()) {
+
+                        }
+                        put(materialFamily, TYPE_FAMILIES.get(blockType).get(materialFamily));
+                    }
+            }});
+
+    /**
      * Stores block families according to type
      */
     public static Map<BlockTypes, Map<MaterialFamilies, List<DeferredBlock<EmbrasureBlock>>>> TYPE_FAMILIES =
@@ -18,14 +31,6 @@ public record Maps() {
                     put(BlockTypes.EMBRASURE, EMBRASURE_FAMILIES);
             }});
 
-    /**
-     * Stores block families according to material
-     */
-    public static Map<MaterialFamilies, Map<BlockTypes, List<DeferredBlock<EmbrasureBlock>>>> MATERIAL_FAMILIES =
-            Collections.unmodifiableMap(new EnumMap<>(MaterialFamilies.class) {{
-                put(MaterialFamilies.STONE, STONE_EMBRASURES);
-
-            }});
 
     public static Map<MaterialFamilies, List<DeferredBlock<EmbrasureBlock>>> EMBRASURE_FAMILIES =
             Collections.unmodifiableMap(new EnumMap<>(MaterialFamilies.class) {{
